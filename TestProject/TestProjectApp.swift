f1c7fcce3c01f260f7ecc36b6a771800241b806f
Nikita Gorbunov <gorbunovna@bcsmarkets>
2023-06-16 17:11:39 +0300

    initial commit

---
+++ TestProject/TestProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestProjectApp.swift
+//  TestProject
+//
+//  Created by Nikita Gorbunov on 16.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct TestProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
